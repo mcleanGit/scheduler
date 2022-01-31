46d763331448ea71f17f68d08ceebc83aece5b78
Don McLean <mclean@utoronto.ca>
2022-01-30 21:46:49 -0500

    final revisions to date

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
+## Project Purpose
+Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+*Motivation.* As an executive leader for many years in higher-education and innovation in the performing arts and digital media, I am driven by the socio-economic, organizational, and technological impact of these transformative fields. Now that my institutional administrative role is completed, I am reengaged in many ongoing projects in interdisciplinary research and infrastructure development. In addition to their legacy value, some of those projects also explore new horizons, in part related to my role as co-founder of Digital Media at the Crossroads (DM@X) and other networking initiatives. I am motivated to expand my circle of influence and range of capability by working from the ground up, to explore the coding and implementation aspects of the digital present and future as an important complement to my executive experience in these areas. Although at this stage of my career I am more likely to be on the employer side of the hiring table, I am interested in developing my coding skills and perspective on this side of the industry. The current Work Day Scheduler application is part of that process.
+
+The current Scheduler app, certainly in the form I have it here, would be quite inadequate as a real-life application. For one thing, in my previous professional role, the day's meetings routinely began at 8 am and concerts, normally about 5 evenings a week, did not end until after 10 pm. Accordingly, the Scheduler has been adjusted to encompass those hours, 0800-2200. It would also be necessary to flip between daily, weekly, and monthly schedules to be able to plan ahead, and reflect or review back. These elements are beyond the scope of this excercise.
+
+
+## Aspects of the Project
+
+The sequence of the application is as follows:
+- the planner is opened in the browser
+- the current day is displayed for extended business hours, to reflect my own experience
+- the schedule presents hourly timeblocks
+- the timeblocks are coded to indicate whether the hour is in the past grey, present--the current hour red--, or future green
+- clicking a timeblock allows entry of text describing the event
+- the save button at the right of each hourly timeblock saves the info in local storage
+- so that the saved events persist when the page is refreshed
+
+- note: I was unable to utilize the diskette save-icon in the right-hand column, despite using external resources and confirming that it was present in the added cdn resources. To review ?
+- note: it would be beneficial to include a delete or clear button for individual hourly slots or for the day-page as a whole, as shown, but this functinaility has not yet been fully implemented here
+- note: many difficulties arose from my decision to try to drive the HTML via JS, jQuery, and Moment.js. A good learning experience but it would, in hindsight, have been wiser to do most of the layout directly in the HTML, which would reduce the complexity of the JS and provide me with greater independence from often-contradictory external resources at this stage of my development.
+
+
+## Review
+
+The URL of the functional, deployed application.
+URL:
+
+The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+URL:
+
+
+
+
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 The starter code uses the [Moment.js](https://momentjs.com/) library to work with date and time, but you're free to use a different JavaScript solution to handle this functionality because Moment.js is considered a "legacy" product. Learn more about these other solutions in the [Moment.js project status page.](https://momentjs.com/docs/#/-project-status/).
+
 
 ## User Story
 
@@ -32,7 +68,6 @@ THEN the saved events persist
 
 The following animation demonstrates the application functionality:
 
-![Work Day Scheduler app with color-coded time slots shows a new event being typed in the 5PM slot.](./Assets/05-third-party-apis-homework-demo.gif)
 
 ## Review
 
--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-/* script.js for Scheduler */
-// time block for jumbotron -- works!  (now doesn't)
+/* script.js for Scheduler : much third-party with modifications as trying to push dynamic JS skillset */
 class TimeblockObj {
   constructor(hour, todo) {
     this.hour = hour;
     this.todo = todo; 
   }
 }
-// superseded method use $( document ).ready ?? see Stack Overflow
-window.onload = function() {
+// $(document).ready(function ()) preferred to window.onload; neither should not be necessary if all js below body ??
+$(document).ready(function () {
   const currentTimeblocks = getCurrentTimeblocks();
   const currentTime = moment();
 
   displayCurrentDate(currentTime);
-  displayTimeBlockRows(currentTime);
+  displayTimeblockRows(currentTime);
 
   document.querySelector(".container")
     .addEventListener("click", function(event) {
       containerClicked(event, currentTimeblocks);
     });
-  setTimeblockText(currentTimeblocks);
-};
+    setTimeblockText(currentTimeblocks);
+});
 
+// to review localStorage syntax here and later; functions with console.log for empty array, to check saved content
 function getCurrentTimeblocks() {
   const currentTimeblocks = localStorage.getItem("timeblockObjects");
   return currentTimeblocks ? JSON.parse(currentTimeblocks) : [];
 }
 
+// this works in correct drm format to this point - OK.
 function displayCurrentDate(currentTime) {
   document.getElementById("currentDay")
-    .textContent = currentTime.format("dddd, MMMM Do YYYY, HH:mm");
+    .textContent = currentTime.format("dddd, MMMM Do, YYYY, HH:mm");
 }
 
-/*
-// timeblock rows display function begins here
-function displayTimeBlockRows(currentTime) {
+// timeblock rows display function begins here; to review flex aspects, row shows < 12 columns wide
+function displayTimeblockRows(currentTime) {
   const currentHour = currentTime.hour();
-  // hours initially will be 9-5, 9-17; to revise later as 7-22
-  // creates col layout of 1+10+1; could be revised depending on look
-  for (let i=9; i <= 17; i++) {
+  // hours requisitie 9-5, 9-17; revised as 08-22; timeblock is row
+  // creates col bootstrap layout of 1+10+1; could be revised depending on look; 10 not functioning properly ??
+  for (let i = 8; i <= 22; i ++) {
     const timeblock = createTimeblockRow(i);
     const hourCol = createCol(createHourDiv(i), 1);
     const textArea = createCol(createTextArea(i, currentHour), 10);
@@ -46,19 +46,20 @@ function displayTimeBlockRows(currentTime) {
     document.querySelector(".container").appendChild(timeblock);
   }
 }
-//timeblock rows create function
+
+//timeblock rows create function, note use of ${} for Id, shortform for ...getElementbyId etc.
   function createTimeblockRow(hourId) {
     const timeblock = document.createElement("div");
     timeblock.classList.add("row");
-    timeblock.id = "timeblock-${hourId}";
+    timeblock.id = `timeblock-${hourId}`;
     return timeblock;
   }
 
-// timeblock cols create function
+// timeblock cols create function, similar use of ${} syntax
   function createCol(element, colSize) {
     const col = document.createElement("div");
-    col.classList.add("col-${colSize}", "p-0");
-    col.appendChild(elememnt);
+    col.classList.add(`col-${colSize}`, "p-0");
+    col.appendChild(element);
     return col;
   }
 
@@ -72,7 +73,7 @@ function displayTimeBlockRows(currentTime) {
 
   function formatHour(hour) {
     const hourString = String(hour);
-    return moment(hourString, "H").format("H");
+    return moment(hourString, "HH").format("HH");
   }
 
   function createTextArea(hour, currentHour) {
@@ -86,43 +87,46 @@ function displayTimeBlockRows(currentTime) {
     : hour === currentHour ? "present"
     : "future";
   }
-
+// uses fontawesome API for floppy-disk icon, problematic - not loading ?
   function createSaveBtn(hour) {
     const saveBtn = document.createElement("button");
     saveBtn.classList.add("saveBtn");
-    saveBtn.innerHTML = "<i class='fas fa-save'></i>";
+    saveBtn.innerHTML = '<i class = svg-inline--fa fa-save fa-w-14"></i>';  
+    "svg-inline--fa fa-save fa-w-14" 
     saveBtn.setAttribute("data-hour", hour);
     return saveBtn;
   }
-
-  function appendTimeblockColumns(timeblockRow, hourCol, textAreaCol, saveBtnCol) {
+// this part of the code needs review, third-party research, problematic
+  function appendTimeblockColumns(timeBlockRow, hourCol, textAreaCol, saveBtnCol) {
     const innerCols = [hourCol, textAreaCol, saveBtnCol];
     for (let col of innerCols) {
-      timeblockRow.appendChild(col);
+      timeBlockRow.appendChild(col);
     }
   }
 
-// localStorage saves NB to review
+// localStorage saves NB to review, correct minor errors in syntax
   function containerClicked(event, timeblockList) {
-    if (isSaveButton(event)) {}
+    if (isSaveButton(event)) {
       const timeblockHour = getTimeblockHour(event);
       const textAreaValue = getTextAreaValue(timeblockHour);
       placeTimeblockInList(new TimeblockObj(timeblockHour, textAreaValue, timeblockList));
       saveTimeblockList(timeblockList);   
+    }
   }
 
   function isSaveButton(event) {
-    return event.target.matches("button") || event.target.matches(".fa-save");
+    return event.target.matches("button") || event.target.matches(".fa fa-save");
   }
-
+// to review syntax of this: ? and event.target etc redundancies
   function getTimeblockHour(event) {
-    return event.target.matches(".fa-save") ? event.target.parentElement.dataset.hour : event.target.dataset.hour;
+    return event.target.matches(".fa fa-save") ? event.target.parentElement.dataset.hour : event.target.dataset.hour;
   }
-
+// again use of $ syntax as Id marker
   function getTextAreaValue(timeblockHour) {
-    return document.querySelector("#timeblock-${timeblockHour} textarea").value;
+    return document.querySelector(`#timeblock-${timeblockHour} textarea`).value;
   }
-
+// note that .push seems problematic, adds new item to end of array; length parameter undefined ?
+// the structure of much of this requires further review
   function placeTimeblockInList(newTimeblockObj, timeblockList) {
     if (timeblockList.length > 0) {
       for (let savedTimeblock of timeblockList) {
@@ -145,9 +149,8 @@ function displayTimeBlockRows(currentTime) {
       return;
     } else {
       for (let timeblock of timeblockList) {
-        document.querySelector("#timeblock-${timeblock.hour} textarea")
+        document.querySelector(`#timeblock-${timeblock.hour} textarea`)
         .value = timeblock.todo;
       }
     }
-  }
-  
+  };
